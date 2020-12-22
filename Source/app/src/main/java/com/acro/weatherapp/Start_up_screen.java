da34df7cce6b5f78cabc4381f2b0f7b6081c8815
Siddhant Dua <siddhantdua@hotmail.com>
2020-12-23 02:47:32 +0530

    ViewPager Initiation
    Co-Authored-By: Shambu Valasang <shambuqwerty@gmail.com>

---
+++ Source/app/src/main/java/com/acro/weatherapp/Start_up_screen.java
@@ -0,0 +1,13 @@
+package com.acro.weatherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Start_up_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_screen);
+    }
+}
