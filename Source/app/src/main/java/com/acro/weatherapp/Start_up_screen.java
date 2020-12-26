f5369c935beb398c585628a5d4c45875e77fc1d6
Siddhant Dua <siddhantdua@hotmail.com>
2020-12-26 07:45:43 +0530

    OnBoarding Screen with liquid pager implementation completed

--- Source/app/src/main/java/com/acro/weatherapp/Start_up_screen.java
+++ Source/app/src/main/java/com/acro/weatherapp/Start_up_screen.java
@@ -1,7 +1,12 @@
 package com.acro.weatherapp;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.graphics.Color;
 import android.os.Build;
@@ -12,11 +17,16 @@ import android.widget.TextView;
 
 import com.airbnb.lottie.LottieAnimationView;
 
+
+
 public class Start_up_screen extends AppCompatActivity {
 
     ImageView logo,Splashbg;
     TextView appname;
     LottieAnimationView lottieAnimationView;
+    private static final int NUM_PAGES =3;
+    private ViewPager viewPager;
+    private ScreenSliderPageAdapter pagerAdapter;
 
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
@@ -30,10 +40,42 @@ public class Start_up_screen extends AppCompatActivity {
         Splashbg = findViewById(R.id.splashbg);
         lottieAnimationView = findViewById(R.id.Animation);
 
-        Splashbg.animate().translationY(-2200).setDuration(1000).setStartDelay(2000);
+        viewPager = findViewById(R.id.pager);
+        pagerAdapter = new ScreenSliderPageAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
+
+        Splashbg.animate().translationY(-2500).setDuration(1000).setStartDelay(2000);
         lottieAnimationView.animate().translationY(1800).setDuration(1000).setStartDelay(2000);
-        logo.animate().alphaBy(0).alpha(1).translationY(300).setDuration(1000).setStartDelay(2200);
-        appname.animate().alpha(0).alphaBy(1).translationY(300).setDuration(1000).setStartDelay(2200);
+
         
+    }
+    private class ScreenSliderPageAdapter extends FragmentStatePagerAdapter{
+
+
+        public ScreenSliderPageAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    OnboardingFragment1 tab1 = new OnboardingFragment1();
+                    return tab1;
+                case 1:
+                    OnboardingFragment2 tab2 = new OnboardingFragment2();
+                    return tab2;
+                case 2:
+                    OnboardingFragment3 tab3 = new OnboardingFragment3();
+                    return tab3;
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
     }
 }
